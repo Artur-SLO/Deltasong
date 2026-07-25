85515719429d0d2b567d6ff7165e66309f907ac6
Artur-SLO <arturvitorcintra@gmail.com>
2026-07-25 20:29:38 -0300

    feat: guessed characters set

--- frontend/src/core/Game.js
+++ frontend/src/core/Game.js
@@ -2,32 +2,37 @@ import Character from './Character.js';
 
 export default class Game {
     constructor(json) {
-        this.characters = [];
-        json.forEach(character => {
-            this.characters.push(new Character(character));
-        });
-
+        this.characters = json.map(character => new Character(character));
         this.totalCharacters = this.characters.length;
         this.target = this.characters[Math.floor(Math.random() * this.totalCharacters)];
+        this.guessedNames = new Set();
     }
 
     makeGuess(name) {
         if (!name) throw new Error("Empty name");
 
-        name = name.toUpperCase();
-        const guess = this.characters.find(c => c.getName() === name);
+        name = name.toUpperCase().trim();
 
+        const guess = this.characters.find(c => c.getName() === name);
         if (!guess) throw new Error("Non-existent character");
+
+        if (this.guessedNames.has(name)) {
+            throw new Error("Character already guessed!");
+        }
+        this.guessedNames.add(name);
 
         const result = this.target.compareTo(guess);
         const victory = this.#checkResult(result);
-
         return victory ? "Victory" : result;
     }
 
     #checkResult(result) {
         const keys = Object.keys(result).filter(k => k !== 'image');
         return keys.every(key => result[key].correct === true);
+    }
+
+    getGuessedNames() {
+        return Array.from(this.guessedNames);
     }
 
     print() {
